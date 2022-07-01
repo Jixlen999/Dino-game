efdc618359371e6ae7f7e77a379bbb20f7826a84
Victoria <viktoriyaivanyutenko@yandex.ru>
2022-07-01 18:14:45 +0300

    heroes upgrade

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 dino = document.getElementById("dino");
 cactus = document.getElementById("cactus");
+dino.classList.add("moving");
 
-document.addEventListener("keydown", function(event) {
-    jump();
+document.addEventListener("keydown", function (event) {
+	jump();
 });
 
-
 function jump() {
-    if (dino.classList != "jump") {
-        dino.classList.add("jump")
-    }
-    setTimeout(function() {
-        dino.classList.remove("jump")
-    }, 350)
+	if (dino.classList != "jump") {
+		dino.classList.remove("moving");
+		dino.classList.add("jump");
+	}
+	setTimeout(function () {
+		dino.classList.remove("jump");
+		dino.classList.add("moving");
+	}, 1000);
 }
 
-let isAlive = setInterval(function() {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+let isAlive = setInterval(function () {
+	let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+	let cactusLeft = parseInt(
+		window.getComputedStyle(cactus).getPropertyValue("left")
+	);
 
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("Game Over")
-    }
-}, 10)
+	if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 170) {
+		alert("Game Over");
+	}
+}, 10);
